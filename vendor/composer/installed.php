a5f14e2bcb550685080499ae9dc5855ac6afd99e
Nicolas Duflot <nduflot@jehann.fr>
2020-11-16 17:04:50 +0100

    ajout de vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '770a84795796353faa76bb97ff02ed2c3892aa47',
+    'name' => 'nduflot/php-avance-2020-11-16',
+  ),
+  'versions' => 
+  array (
+    'doctrine/collections' => 
+    array (
+      'pretty_version' => '1.6.7',
+      'version' => '1.6.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '55f8b799269a1a472457bd1a41b4f379d4cfba4a',
+    ),
+    'nduflot/php-avance-2020-11-16' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '770a84795796353faa76bb97ff02ed2c3892aa47',
+    ),
+  ),
+);

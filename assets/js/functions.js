04bd13bebcbef7ee44c8d0589de3527f8a806cf8
Nicolas Duflot <nduflot@jehann.fr>
2020-11-18 11:12:44 +0100

    suppression par modal

---
+++ assets/js/functions.js
@@ -0,0 +1,6 @@
+function loaded(callable){document.addEventListener("DOMContentLoaded", callable);}
+function $(selector){return document.querySelector(selector);}
+function $$(selector){return document.querySelectorAll(selector);}
+loaded(function(){
+
+});
--- pdo.php
+++ pdo.php
@@ -154,6 +154,11 @@ use Doctrine\Common\Collections\ArrayCollection;
                                         Modifier
                                     </button>
                                 </a>
+                                <button type="button" class="btn btn-danger"
+                                        data-toggle="modal" data-target="#exampleModal"
+                                        data-idJeux="<?php echo $donnees["ID"]; ?>">
+                                    Supprimer
+                                </button>
                             </td>
                         </tr>
                     <?php
@@ -161,6 +166,25 @@ use Doctrine\Common\Collections\ArrayCollection;
                     ?>
                     </tbody>
                 </table>
+            </div>
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
             </div>
             <?php
             $response->closeCursor();

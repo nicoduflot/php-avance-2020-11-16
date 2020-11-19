0dbdc4fc459666802cdd5921d87b0ca23663738c
Nicolas Duflot <nduflot@jehann.fr>
2020-11-19 08:03:26 +0100

    jeu de combat correction simule aléatoire fin dès qu'un des combattant est hors de combat

--- classesetendues.php
+++ classesetendues.php
@@ -28,6 +28,14 @@ use Gam\Guerrier;
         <article class="col-lg-12">
             <h1>Classes Etendues</h1>
             <h2>Principes</h2>
+            <p>
+                Une est étendue quand elle possède une classe fille. La classe fille
+                hérite automatiquement des attributs et des méthodes de la classe mère. L'avantage
+                est que la classe fille peut posséder ses propres méthodes et attributs, mais elle peut aussi
+                surcharger les méthodes de la classe mère en les redéfinissants. Mais si on veut pouvoir redéfinir,
+                par exemple, les getters ou setters de la classe mère, les attributs concernés dans la classe
+                mère doivent être alors déclarés en protected et plus en private.
+            </p>
             <pre>
                 <?php
                 $perso1 = new Mage("Froideval", new Arme("Epée", 2));
@@ -51,11 +59,16 @@ use Gam\Guerrier;
                     }else{
                         $perso2->frapper($perso1);
                     }
-
+                    if(!$perso2->checkVitality($perso1)){
+                        exit();
+                    }
                     if(random_int(1, 3)== 3){
                         $perso1->bouleDeFeu($perso2);
                     }else{
                         $perso1->frapper($perso2);
+                    }
+                    if(!$perso1->checkVitality($perso2)){
+                        exit();
                     }
                 }
                 ?>

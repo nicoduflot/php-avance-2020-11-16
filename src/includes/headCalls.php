5dc41f1114437eb5959e44e2fa9ad5a3c1d0a5a5
Nicolas Duflot <nduflot@jehann.fr>
2020-11-20 16:54:19 +0100

    breadcrumb modif bootstrap

--- src/includes/headCalls.php
+++ src/includes/headCalls.php
@@ -2,4 +2,5 @@
 <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
 <script src="./assets/js/bootstrap.bundle.min.js"></script>
 <script src="./assets/js/functions.js"></script>
+<link rel="stylesheet" href="./assets/css/style.css">
 <?php
--- src/includes/navigation.php
+++ src/includes/navigation.php
@@ -30,3 +30,9 @@
         </ul>
     </div>
 </nav>
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Home</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Library</li>
+    </ol>
+</nav>
